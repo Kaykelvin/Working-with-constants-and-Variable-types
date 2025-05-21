a428c8b1759c10fd633d7c0c0ae660aa531b22ff
Kabalikelvin12345@gmail.com <kabalikelvin12345@gmail.com>
2025-05-21 12:25:43 +0300

    branch3: second commit

--- CURRENCYE_EXCHANGE.c
+++ CURRENCYE_EXCHANGE.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+const double USD_TO_UGX = 3500; //Constant exchange rate //Global variable
+
+int main() {
+    double USD_Amount = 700.0;  // Local variable
+    double UGXNeeded = USD_Amount * USD_TO_UGX;
+
+    printf("Currency Exchange Calculation:\n");
+    printf("Exchange Rate: %.2lf UGX/USD\n", USD_TO_UGX);
+    printf("USD Needed: %.2lf\n", USD_Amount);
+    printf("UGX Required: %.2lf UGX\n", UGXNeeded);
+
+    return 0;
+}
