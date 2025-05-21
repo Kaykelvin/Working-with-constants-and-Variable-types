48c376b07ccfb9710fbb47e0cd3284994e68ed1a
Kabalikelvin12345@gmail.com <kabalikelvin12345@gmail.com>
2025-05-21 11:58:40 +0300

    branch1: second commit

--- 1.SIMPLE_INTEREST.c
+++ 1.SIMPLE_INTEREST.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+double principal = 20000;  // Global variable
+double time = 3;            // Global variable
+const double Interest_Rate = 0.15;  // Global variable
+
+int main() {
+    double SimpleInterest = principal * time * Interest_Rate; //Local variable
+
+    printf("Simple Interest Calculation:\n");
+    printf("Principal: %.2lf UGX\n", principal);
+    printf("Time: %.2lf years\n", time);
+    printf("Interest Rate: %.2lf%%\n", Interest_Rate * 100);
+    printf("Simple Interest: %.2lf UGX\n", SimpleInterest);
+
+    return 0;
+}
